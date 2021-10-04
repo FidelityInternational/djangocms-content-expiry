6aa65c1442e0bfa4bf347edd050b1e8c8fa8d5d4
bernard <bvdv9075@gmail.com>
2021-10-04 10:20:48 +0100

    updated installed apps with rangefilter entry

--- aldryn_config.py
+++ aldryn_config.py
@@ -3,6 +3,9 @@ from aldryn_client import forms
 
 class Form(forms.BaseForm):
     def to_settings(self, data, settings):
-        settings['INSTALLED_APPS'].extend(['djangocms_content_expiry'])
+        settings['INSTALLED_APPS'].extend([
+            'djangocms_content_expiry',
+            'rangefilter',
+        ])
         settings["DJANGOCMS_CONTENT_EXPIRY_ENABLED"] = True
         return settings
