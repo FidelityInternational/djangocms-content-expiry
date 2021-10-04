e1a45b8dc19afdc3ac5cd5b8c698773d2d5437c0
bernard <bvdv9075@gmail.com>
2021-10-04 10:03:45 +0100

    created release branch

---
+++ aldryn_config.py
@@ -0,0 +1,8 @@
+from aldryn_client import forms
+
+
+class Form(forms.BaseForm):
+    def to_settings(self, data, settings):
+        settings['INSTALLED_APPS'].extend(['djangocms_content_expiry'])
+        settings["DJANGOCMS_CONTENT_EXPIRY_ENABLED"] = True
+        return settings
